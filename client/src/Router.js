ebb192df3a2d604d7e5cde8b96608f63ea2e3493
yaelBrown <yaelrbrown@gmail.com>
2021-03-25 10:19:13 -0500

    added link to dashboard

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,15 +3,17 @@ import { Switch, Route } from "react-router-dom"
 
 import Home from "./Views/Home"
 import Test from "./Views/Test"
+import Dashboard from "./Views/Dashboard"
 
 export default class Router extends Component {
-    render() {
-        return (
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/test" exact component={Test}/>
-                <Route component={Error}/>
-            </Switch>
-        )
-    }
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/test" exact component={Test}/>
+        <Route path="/dashboard" exact component={Dashboard}/>
+        <Route component={Error}/>
+      </Switch>
+    )
+  }
 }
